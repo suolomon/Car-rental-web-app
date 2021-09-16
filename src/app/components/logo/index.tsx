083b8a0fda382f91314b7503e8256adc988b87d6
Solomon Talemwa <adamtblack96@gmail.com>
2021-09-16 23:56:46 +0300

    Add footer to homepage of website

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 import CarLogoImg from '../../../assets/images/car-logo.png';
+import CarLogoDarkImg from '../../../assets/images/car-logo-dark.png';
 
+interface ILogoProps {
+    color?: 'white' | 'dark';
+    bgColor?: 'white' | 'dark';
+}
 const LogoContainer = styled.div `
 ${tw`
 flex
@@ -18,7 +23,8 @@ font-bold
 text-black
 m-1
 `}
-`
+${({color}: any) => color === 'white'? tw`text-white` : tw`text-black`}
+`as any;
 
 const Image = styled.div`
 ${tw`
@@ -30,13 +36,14 @@ img{
     height: 100%;
 }
 `
-export function Logo(){
+export function Logo(props:  ILogoProps){
+    const {color, bgColor} = props
     return (
         <LogoContainer>
             <Image>
-                <img src={CarLogoImg}/>
+                <img src={bgColor === 'dark' ? CarLogoDarkImg : CarLogoImg }/>
             </Image>
-            <LogoText>RentIt.</LogoText>
+            <LogoText color={color || 'dark'}>RentIt.</LogoText>
         </LogoContainer>
     )
 }
